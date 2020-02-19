76511eea21415586533edcb14997ef6b48f9d670
Lina <mm1969pr@gmail.com>
2020-02-19 23:57:18 +0200

    Adding WebSocket & Chat Demo Model

--- src/main/java/com/alevel/finalProject/Doka/Doka/domain/Message.java
+++ src/main/java/com/alevel/finalProject/Doka/Doka/domain/Message.java
@@ -1,4 +1,49 @@
 package com.alevel.finalProject.Doka.Doka.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+@Getter
+@Setter
+@Document(collection = "messages")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "messageId")
+    private String messageId;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "senderId")
+    private String senderId;
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public Message() {
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Message[message_id=%s, text='%s', senderId='%s']",
+                messageId, text, senderId);
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
 }
