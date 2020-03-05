2d9f7fd7ca6393292679294545b8cf3e36045213
LolenkoAndrew <lolenko.aa@gmail.com>
2020-03-05 09:34:47 +0200

    add test video group call with kurento, from now to start this app need kurento media server

--- src/main/java/com/alevel/finalProject/Doka/Doka/DokaApplication.java
+++ src/main/java/com/alevel/finalProject/Doka/Doka/DokaApplication.java
@@ -1,13 +1,46 @@
 package com.alevel.finalProject.Doka.Doka;
 
+import com.alevel.finalProject.Doka.Doka.kurento.CallHandler;
+import com.alevel.finalProject.Doka.Doka.kurento.RoomManager;
+import com.alevel.finalProject.Doka.Doka.kurento.UserRegistry;
+import org.kurento.client.KurentoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
 
 @SpringBootApplication
-public class DokaApplication {
+@EnableWebSocket
+public class DokaApplication  implements WebSocketConfigurer {
+	@Bean
+	public UserRegistry registry() {
+		return new UserRegistry();
+	}
+
+	@Bean
+	public RoomManager roomManager() {
+		return new RoomManager();
+	}
+
+	@Bean
+	public CallHandler groupCallHandler() {
+		return new CallHandler();
+	}
+
+	@Bean
+	public KurentoClient kurentoClient() {
+		return KurentoClient.create();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DokaApplication.class, args);
 	}
 
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(groupCallHandler(), "/groupcall");
+	}
 }
