1dbc903c2e3409ff20b014ae5501ad0cdba4b5e4
LolenkoAndrew <lolenko.aa@gmail.com>
2020-03-25 00:40:40 +0200

    update some freemarker (bootstrap) parts and now it looks better

--- src/main/java/com/alevel/finalProject/Doka/Doka/config/WebSecurityConfig.java
+++ src/main/java/com/alevel/finalProject/Doka/Doka/config/WebSecurityConfig.java
@@ -30,7 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/reg").permitAll()
+                .antMatchers("/", "/reg", "/resources/**", "/static/**", "/webjars/**", "/css/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
